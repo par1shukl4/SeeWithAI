46381e1d6729bc0d66caa66dd75a373432950509
nityawav7 <nityan5215@gmail.com>
2025-07-09 17:04:31 +0530

    Removed all files from GitHub repo except .gitignore

--- test_play.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run([
-    r"C:\Users\Nitya Nand\Desktop\office\chatterbot\ffmpeg-7.1.1-essentials_build\ffmpeg-7.1.1-essentials_build\bin\ffplay.exe",
-    "-nodisp", "-autoexit", "voice_output.mp3"
-])
